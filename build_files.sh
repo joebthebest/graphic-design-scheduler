afcffe6972ab7f064ddcf299c672c8c01cddd0a3
James Emmaedeh <jamesemmaedeh@gmail.com>
2026-08-19 10:43:16 +0100

    Optimize image assets, remove staticfiles from git index, and add .vercelignore to fix Vercel function bundle size

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-echo "Installing project dependencies..."
-python3 -m pip install -r requirements.txt
-
-echo "Collecting static assets..."
-python3 manage.py collectstatic --no-input --clear
-
-echo "Static build complete."
+pip install -r requirements.txt
+python manage.py collectstatic --no-input
